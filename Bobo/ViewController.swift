d5626ec9709ac1443d374635569a4cf547ed33ed
Azura Sakan Taufik <azstima@gmail.com>
2021-04-29 05:38:29 +0700

    Initial Commit

---
+++ Bobo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bobo
+//
+//  Created by Azura Sakan Taufik on 29/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
